bb688b1c7a7dfd5c814fda54adde506abc9bb4e5
Documenter.jl <documenter@juliadocs.github.io>
2026-01-19 07:21:19 +0000

    build based on b35793f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DatabasePlayground","category":"page"},{"location":"#DatabasePlayground","page":"Home","title":"DatabasePlayground","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DatabasePlayground.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DatabasePlayground]","category":"page"}]
+[{"location":"#DatabasePlayground","page":"Home","title":"DatabasePlayground","text":"Documentation for DatabasePlayground.\n\n","category":"section"}]
 }
